94bd766a8cbdcb97454b38328dd69bd430c9d7c5
xiewendan <stephenxjc@gmail.com>
2018-05-19 22:51:18 +0800

    [c# helloworld] c# 示例

---
+++ c#/HelloWorld.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace c_
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            string szMsg = "Hello World!";
+            Console.WriteLine(szMsg);
+        }
+    }
+}
