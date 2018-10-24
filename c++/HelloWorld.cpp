92850cf9c60093f0d4ffa60d52dc89eb6b8c1778
xiaobao <jinchunxie@126.com>
2018-10-24 14:52:28 +0800

    初次提交

---
+++ c++/HelloWorld.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "hello world! by xjc" << std::endl;
+    return 1;
+}
---
+++ python/HelloWorld.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+# __author__ = xiaobao
+# __date__ = 2018/07/05 12:52:08
+
+# desc: xcj
+print("xjc")
